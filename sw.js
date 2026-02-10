d7730aabcba010586fd471dfbe60704f9830020e
1809240jerick-boop <1809240jerick@gmail.com>
2026-02-09 12:43:48 -1200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // --- 1. UPDATE VERSION HERE ---
-const CACHE_NAME = 'rbi-system-v10'; 
+const CACHE_NAME = 'rbi-system-v11'; 
 
 // 2. Install: Safe caching strategy
 self.addEventListener('install', (event) => {
@@ -60,6 +60,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
 
 
